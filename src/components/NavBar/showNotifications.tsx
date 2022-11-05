119c4cb941384f261b4f13a310a71a857adf10ba
Dhruv agarwal <dhruv@Dhruvs-MBP.mshome.net>
2022-11-05 17:35:45 +0530

    Push protocol uiEmbed added

---
+++ src/components/NavBar/showNotifications.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useContext } from 'react';
+import { EmbedSDK } from "@pushprotocol/uiembed";
+import { Web3Context } from './web3Context';
+
+const showNotifications = () => {
+    const { account, chainId } = useContext<any>(Web3Context);
+
+    useEffect(() => {
+        if (account) { // 'your connected wallet address'
+          EmbedSDK.init({
+            chainId,
+            headerText: 'Hello Hacker Dashboard', // optional
+            targetID: 'sdk-trigger-id', // mandatory
+            appName: 'hackerApp', // mandatory
+            user: account, // mandatory
+            viewOptions: {
+                type: 'sidebar', // optional [default: 'sidebar', 'modal']
+                showUnreadIndicator: true, // optional
+                unreadIndicatorColor: '#cc1919',
+                unreadIndicatorPosition: 'top-right',
+            },
+            theme: 'light',
+            onOpen: () => {
+              console.log('-> client dApp onOpen callback');
+            },
+            onClose: () => {
+              console.log('-> client dApp onClose callback');
+            }
+          });
+        }
+    
+        return () => {
+          EmbedSDK.cleanup();
+        };
+      }, [account, chainId]);
+
+
+    return (
+        <>
+          <button id="sdk-trigger-id">Show Notifications</button>
+        </>
+    );
+}
+
+export default EmbedPage;
